fe35ed57b343038f8eb5290dc29f5c6c02db12d5
Ben Limmer <ben@benlimmer.com>
2025-01-20 16:52:53 -0700

    feat: add provider implementation

---
+++ test/GithubActionsIdentityProvider.test.ts
@@ -0,0 +1,69 @@
+import { App, Stack } from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { GithubActionsIdentityProvider } from "../src";
+
+describe("GithubActionsIdentityProvider", () => {
+  it("creates the proper URL", () => {
+    const app = new App();
+    const stack = new Stack(app, "TestStack");
+    new GithubActionsIdentityProvider(stack, "GithubActionsIdentityProvider");
+
+    Template.fromStack(stack).hasResourceProperties("AWS::IAM::OIDCProvider", {
+      Url: "https://token.actions.githubusercontent.com",
+    });
+  });
+
+  it("uses the organization ID as the client ID", () => {
+    const app = new App();
+    const stack = new Stack(app, "TestStack");
+    new GithubActionsIdentityProvider(stack, "GithubActionsIdentityProvider");
+
+    Template.fromStack(stack).hasResourceProperties("AWS::IAM::OIDCProvider", {
+      ClientIdList: ["sts.amazonaws.com"],
+    });
+  });
+
+  it("uses a default thumbprint list", () => {
+    const app = new App();
+    const stack = new Stack(app, "TestStack");
+    new GithubActionsIdentityProvider(stack, "GithubActionsIdentityProvider");
+
+    Template.fromStack(stack).hasResourceProperties("AWS::IAM::OIDCProvider", {
+      ThumbprintList: ["d89e3bd43d5d909b47a18977aa9d5ce36cee184c"],
+    });
+  });
+
+  it("can override the thumbprint list", () => {
+    const app = new App();
+    const stack = new Stack(app, "TestStack");
+    new GithubActionsIdentityProvider(stack, "GithubActionsIdentityProvider", {
+      thumbprints: ["thumbprint"],
+    });
+
+    Template.fromStack(stack).hasResourceProperties("AWS::IAM::OIDCProvider", {
+      ThumbprintList: ["thumbprint"],
+    });
+  });
+
+  it("can import an existing provider", () => {
+    const app = new App();
+    const stack = new Stack(app, "TestStack", {
+      env: { account: "123456789012", region: "us-west-2" },
+    });
+    const provider = GithubActionsIdentityProvider.fromAccount(stack);
+
+    expect(provider.arn).toEqual(
+      `arn:${Stack.of(stack).partition}:iam::123456789012:oidc-provider/token.actions.githubusercontent.com`,
+    );
+  });
+
+  it("can import an existing provider when the stack is environment agnostic", () => {
+    const app = new App();
+    const stack = new Stack(app, "TestStack");
+    const provider = GithubActionsIdentityProvider.fromAccount(stack);
+
+    expect(provider.arn).toEqual(
+      `arn:${Stack.of(stack).partition}:iam::${Stack.of(stack).account}:oidc-provider/token.actions.githubusercontent.com`,
+    );
+  });
+});

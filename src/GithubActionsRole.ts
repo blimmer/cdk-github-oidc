4347ea6be42e6af7baeb1e69d801f5cfc49d5379
Ben Limmer <ben@benlimmer.com>
2025-01-20 17:27:41 -0700

    feat: implement subject on role

--- src/GithubActionsRole.ts
+++ src/GithubActionsRole.ts
@@ -3,6 +3,7 @@ import type { Construct } from "constructs";
 import { RoleProps } from "./generated/IamRoleProps";
 import { type IGithubActionsIdentityProvider } from "./IGithubActionsIdentityProvider";
 import { GithubActionsIdentityProvider } from "./GithubActionsIdentityProvider";
+import type { IGithubActionOidcFilter } from "./filters/IGithubActionOidcFilter";
 
 export interface GithubActionsRoleConfiguration {
   /**
@@ -13,13 +14,31 @@ export interface GithubActionsRoleConfiguration {
    * There can be only one (per AWS Account).
    */
   readonly provider: IGithubActionsIdentityProvider;
+
+  /**
+   * Subject filters to apply to the Github Actions OIDC token.
+   *
+   * This filters restrict which repo/branch/tag/etc. can assume the role. This construct
+   * exposes many common filters, but you can also pass a custom filter if you need to.
+   *
+   * For a basic starting point, you can allow all branches to access the role via:
+   *
+   * const subjectFilters = [
+   *   new BranchFilter({ owner: "my-org", repository: "my-repo", branch: "*" }),
+   * ]
+   */
+  readonly subjectFilters: IGithubActionOidcFilter[];
 }
 
 export interface GithubActionsRoleProps extends GithubActionsRoleConfiguration, RoleProps {}
 
 export class GithubActionsRole extends Role {
   constructor(scope: Construct, id: string, props: GithubActionsRoleProps) {
-    const { provider, ...roleProps } = props;
+    const { provider, subjectFilters, ...roleProps } = props;
+
+    if (subjectFilters.length === 0) {
+      throw new Error("You must provide at least one subject filter");
+    }
 
     super(scope, id, {
       assumedBy: new OpenIdConnectPrincipal(
@@ -29,7 +48,9 @@ export class GithubActionsRole extends Role {
         // make TypeScript happy with the types.
         OpenIdConnectProvider.fromOpenIdConnectProviderArn(scope, `GithubActionsOidcProviderImport${id}`, provider.arn),
         {
-          // TODO: add filters
+          StringLike: {
+            [`${GithubActionsIdentityProvider.issuer}:sub`]: subjectFilters.map((filter) => filter.toSubject()),
+          },
           StringEquals: {
             // Audience is always sts.amazonaws.com with AWS official Github Action
             // https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services#adding-the-identity-provider-to-aws
